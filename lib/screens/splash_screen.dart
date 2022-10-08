d4050ee81413631b5098541994162509fddfe4c9
Taiwo Boluwatife <blyncnov@Taiwos-MBP.lan>
2022-10-08 16:54:35 +0100

    Code Refactor for base layout

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        title: const Text(""),
+      ),
+      body: const Center(
+        child: Text("Hello Spalsh Screeen"),
+      ),
+    );
+  }
+}
